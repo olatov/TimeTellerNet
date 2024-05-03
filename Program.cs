6e2e92e54d838df447f61f4fb0eb3f7d5ac46ce8
Oleg Latov <oleg@tgadigital.com>
2024-05-03 12:44:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTellerNet
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Hello.");
+
+            var interval = TimeSpan.FromMinutes(5);
+
+            while (true)
+            {
+                Console.WriteLine($"It's {DateTime.Now} now.");
+                Console.WriteLine($"Will get back to you in {interval.TotalMinutes} mins.");
+                Console.WriteLine();
+
+                await Task.Delay(interval);
+            }
+        }
+    }
+}
